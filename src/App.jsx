84c49002ca60c130162a9a0688d7cbf6327d0a3e
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-12-16 12:34:03 +1100

    Remove unsed code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from 'react'
-
 import './App.css'
 import Home from './pages/Home'
 import Shop from './pages/Shop'
-import Cart from './pages/Cart'
 
 function App() {
  
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,36 +1,39 @@
 // import { useEffect } from "react"
 // import Shop from "./Shop"
 
-
-const Cart = ({products}) => {
- const cartItems = products.filter(product => product.inCart > 0)
- let priceArray = []
- console.log('Cart Items from Cart Component')
- console.log(cartItems)
-    return(
-        cartItems.length>0 ? (
-            <div>
-                <div>
-                {cartItems.map(item => {
-                    priceArray.push(item.price*item.inCart)
-                    return (
-                        <div key={item.id}>
-                            <p><span><strong>{item.title}:</strong></span> ${item.price*item.inCart} ({item.inCart})</p>
-                        </div>
-                    )
-                })
-}
-                </div>
-                    <div><span><strong>Total:</strong></span>{priceArray.reduce((total, value)=>total+value)}</div>
+const Cart = ({ products }) => {
+  const cartItems = products.filter((product) => product.inCart > 0);
+  let priceArray = [];
+  return cartItems.length > 0 ? (
+    <div>
+      <div>
+        {cartItems.map((item) => {
+          priceArray.push(item.price * item.inCart);
+          return (
+            <div key={item.id}>
+              <p>
+                <span>
+                  <strong>{item.title}:</strong>
+                </span>{" "}
+                ${item.price * item.inCart} ({item.inCart})
+              </p>
             </div>
-        ) : (
-            <div>
-            <p>No items in cart</p>
-            </div>
-        )
-    )
+          );
+        })}
+      </div>
+      <div>
+       
+          <strong>Total: $
+       
+        {priceArray.reduce((total, value) => total + value)}
+        </strong>
+      </div>
+    </div>
+  ) : (
+    <div>
+      <p>No items in cart</p>
+    </div>
+  );
+};
 
-
-}
-
-export default Cart
+export default Cart;
