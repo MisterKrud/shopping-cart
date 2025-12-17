491c69a958a490cd9f6b3d912c0be75658499088
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-12-17 15:02:46 +1100

    Styling update to NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router"
+import { Link } from "react-router-dom"
 import styles from "./NavBar.module.css"
 
 const NavBar = ({items, price}) =>{
     return (
         <div className={styles.navContainer}>
         <nav className = {styles.nav}>
-            <Link to = "/home">Home</Link>
-            <Link to = "/shop">Shop</Link>
-            <Link to = "/cart">Cart : {items} - ${price}</Link>
+            <Link to = "/home" className={styles.link}>Home</Link>
+            <Link to = "/shop" className={styles.link}>Shop</Link>
+            <Link to = "/cart" className={styles.link}>Cart : {items} - ${price}</Link>
         </nav>
         </div>
         
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,6 +1,6 @@
 import { useOutletContext } from "react-router"
 import Categories from "./Categories"
-import style from "./Shop.module.css"
+import styles from "./Shop.module.css"
 
 
 const Shop = ()  => {
@@ -17,25 +17,25 @@ const Shop = ()  => {
     return (
         <>
         <Categories products={products} onClick={handleFilter}/>
-        <div>{filteredProducts.map(product => {
+        <div className = {styles.shopContainer}>{filteredProducts.map(product => {
         return(
-            <div className = {style.card}key={product.id}>
+            <div className = {styles.card}key={product.id}>
             
-           <div className={style.imgContainer}>
+           <div className={styles.imgContainer}>
                 
-            <img className={style.img} src={product.image} alt={product.title}  />
-            <div className={style.caption}>{product.description}</div>
+            <img className={styles.img} src={product.image} alt={product.title}  />
+            <div className={styles.caption}>{product.description}</div>
             </div>
        
-            <h5 className={style.productName}>{product.title}</h5>
+            <h5 className={styles.productName}>{product.title}</h5>
             <p>${product.price}</p>
-            <div className= {style.buttonContainer}>
-            <button className={style.minusButton} onClick = {() =>handleDecrement(product.id)}>-</button>
-            <input className={style.input} type="text" inputMode="numeric" value={product.number} onChange={(e)=>handleQuantityChange(e,product.id)} />
-            <button className={style.addButton} onClick = {()=> handleIncrement(product.id)}>+</button>
+            <div className= {styles.buttonContainer}>
+            <button className={styles.minusButton} onClick = {() =>handleDecrement(product.id)}>-</button>
+            <input className={styles.input} type="text" inputMode="numeric" value={product.number} onChange={(e)=>handleQuantityChange(e,product.id)} />
+            <button className={styles.addButton} onClick = {()=> handleIncrement(product.id)}>+</button>
            
             </div>
-            <button className={style.addToCartButton} onClick ={()=> addToCart(product.id)}>Add to cart</button>
+            <button className={styles.addToCartButton} onClick ={()=> addToCart(product.id)}>Add to cart</button>
          
             </div>
         )
